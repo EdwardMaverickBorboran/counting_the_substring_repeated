d19d1c439e8b4c2b6d16bfb32d8418e20f267841
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 18:03:07 +0800

    Done. First code that I finished in one go.

---
+++ repetition_of_substring.py
@@ -0,0 +1,21 @@
+# Create a program that will --
+
+# Find how many times substring “Emma” appears in the given string.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Given:
+# str_x = "Emma is good developer. Emma is a writer"
+
+# Expected results:
+# Emma appeared 2 times
+
+# Given sentence
+str_x = "Emma is good developer. Emma is a writer"
+print(str_x)
+
+# Counting how many time the word Emma is repeated
+count_emma = str_x.count("Emma")
+print("Emma is repeated", count_emma, "times")
